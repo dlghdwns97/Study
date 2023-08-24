809f7aa88d1f1f9445dfce1febcafb4770f8e0f7
dlghdwns97 <73785455+dlghdwns97@users.noreply.github.com>
2023-08-24 10:25:16 +0900

    Create 5. 나머지가 1이 되는 수 찾기.cs

---
+++ 5. 나머지가 1이 되는 수 찾기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        
+        while(n % answer != 1)
+        {
+            answer += 1;
+        }
+        
+        return answer;
+    }
+}
